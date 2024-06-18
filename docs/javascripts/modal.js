505a152ce7d7d77b3e9d99e43eb2af0ff473253c
PierreDillard <7gaspard77@gmail.com>
2024-06-18 15:43:30 +0200

    implement dynamic definitions and keywords

--- docs/javascripts/modal.js
+++ docs/javascripts/modal.js
@@ -1,38 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const openModal = () => {
-        console.log('Opening modal...');
-        const modal = document.getElementById('modal');
-        // const keywordCloud = document.querySelector('.words-cloud');
-        
-        // // Get the bounding rectangle of the keyword cloud
-        // const keywordCloudRect = keywordCloud.getBoundingClientRect();
-        
-        // // Set the position of the modal relative to the keyword cloud
-        // modal.style.top = `${keywordCloudRect.top + window.scrollY}px`;
-        // modal.style.left = `${keywordCloudRect.left + keywordCloudRect.width + 20}px`; // Adjust the offset as needed
-        
-        modal.classList.remove('hidden');
-        modal.style.display = 'block'; 
-        console.log('Modal classes:', modal.classList);
-        console.log('Modal content:', modal.innerHTML);
+document.addEventListener("DOMContentLoaded", function () {
+    const openModal = (keyword, definition) => {
+        const modal = document.getElementById("modal");
+        const modalTitle = document.getElementById("modal-title");
+        const modalDefinition = document.getElementById("modal-definition");
+
+        if (modalTitle && modalDefinition) {
+            // Update the modal title and definition
+            modalTitle.textContent = keyword;
+            modalDefinition.textContent = definition;
+
+            modal.classList.remove("hidden");
+            modal.style.display = "block";
+        } else {
+            console.error('Modal elements not found');
+        }
     };
 
-    document.getElementById('close-modal').addEventListener('click', function () {
-        console.log('Closing modal...');
-        const modal = document.getElementById('modal');
-        modal.classList.add('hidden');
+    document.getElementById("close-modal").addEventListener("click", function () {
+        const modal = document.getElementById("modal");
+        modal.classList.add("hidden");
+        modal.style.display = "none";
     });
 
-    document.getElementById('modal').addEventListener('click', function (event) {
+    document.getElementById("modal").addEventListener("click", function (event) {
         if (event.target === event.currentTarget) {
-            console.log('Closing modal by clicking outside content...');
-            const modal = document.getElementById('modal');
-            modal.classList.add('hidden');
-            modal.style.display = 'none';
+            const modal = document.getElementById("modal");
+            modal.classList.add("hidden");
+            modal.style.display = "none";
         }
     });
 
-    // Export the openModal function to be used in other scripts
     window.openModal = openModal;
 });
-
